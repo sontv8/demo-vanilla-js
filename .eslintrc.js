7fd5a21a3324a3e8f639fcf84e5d69295ac750bb
sontv8 <sontv8@fpt.edu.vn>
2022-02-21 13:41:24 +0700

    init project

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+        semi: ["error","always"],
+        qoute:["error","double"],
+        indent:["error",4],
+        "eol-last":["error", "never"],
+        "linebreak-style":0
+    }
+};
